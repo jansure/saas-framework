0bb5a4022cdf0c5a5827bc3e0efe325557c6ce21
yangpengfei <yangpengfei@cabrtech.com>
2019-02-26 10:07:18 +0800

    修改argsUtil 读取form表单类型的参数

--- lualib/argsUtil.lua
+++ lualib/argsUtil.lua
@@ -5,9 +5,9 @@ local _M = {
 local args = {}
 local file_args = {}
 local is_have_file_param = false
-
+local error_code, error_msg
 --- 以分隔符为准，将字符串分隔为table，table的key为1/2/3等，value为分隔出的字符串
-local function explode (_str, seperator)
+local function explode(_str, seperator)
     local pos, arr = 0, {}
     for st, sp in function()
         return string.find(_str, seperator, pos, true)
@@ -26,16 +26,17 @@ function _M.init_form_args()
         args = ngx.req.get_uri_args()
     elseif "POST" == request_method then
         ngx.req.read_body()
+        local content_type = receive_headers["content-type"]
         --判断是否是multipart/form-data类型的表单
-        if string.sub(receive_headers["content-type"],1,20) == "multipart/form-data;" then
-            is_have_file_param = true
-            content_type = receive_headers["content-type"]
+        if content_type ~= nil and string.sub(content_type, 1, 20) == "multipart/form-data;" then
+            ngx.log(ngx.ERR, "------是multipart/form-data类型的表单------")
             --body_data可是符合http协议的请求体，不是普通的字符串
-            body_data = ngx.req.get_body_data()
+            local body_data = ngx.req.get_body_data()
 
             --请求体的size大于nginx配置里的client_body_buffer_size，则会导致请求体被缓冲到磁盘临时文件里，client_body_buffer_size默认是8k或者16k
             if not body_data then
                 local datafile = ngx.req.get_body_file()
+                --读取请求体数据，若为空则error
                 if not datafile then
                     error_code = 1
                     error_msg = "no request body found"
@@ -45,10 +46,11 @@ function _M.init_form_args()
                         error_code = 2
                         error_msg = "failed to open " .. tostring(datafile) .. "for reading: " .. tostring(err)
                     else
-                        fh:seek("set")
-                        body_data = fh:read("*a")
+                        --fh:seek("set")
+                        --body_data = fh:read("*a")
+                        local filesize = fh:seek("end")
                         fh:close()
-                        if body_data == "" then
+                        if filesize == "0" then
                             error_code = 3
                             error_msg = "request body is empty"
                         end
@@ -58,34 +60,43 @@ function _M.init_form_args()
             local new_body_data = {}
             --确保取到请求体的数据
             if not error_code then
-                local boundary = "--" .. string.sub(receive_headers["content-type"],31)
-                local body_data_table = explode(tostring(body_data),boundary)
-                local first_string = table.remove(body_data_table,1)
+                local boundary = "--" .. string.sub(content_type, 31)
+                --将请求体数据转换为table
+                local body_data_table = explode(tostring(body_data), boundary)
+                local first_string = table.remove(body_data_table, 1)
                 local last_string = table.remove(body_data_table)
-                for i,v in ipairs(body_data_table) do
-                    --查找文件类型的参数
-                    local start_pos,end_pos,capture,capture2 = string.find(v,'Content%-Disposition: form%-data; name="(.+)"; filename="(.*)"')
+                --循环table，处理参数
+                for i, v in ipairs(body_data_table) do
+                    ngx.log(ngx.ERR, "------body_data_table[i]:", body_data_table[i])
+                    --查找文件类型的参数，capture是参数名称，capture2是文件名
+                    local start_pos, end_pos, capture, capture2 = string.find(v, 'Content%-Disposition: form%-data; name="(.+)"; filename="(.*)"')
                     --普通参数
                     if not start_pos then
-                        local t = explode(v,"rnrn")
-                        local temp_param_name = string.sub(t[1],41,-2)
-                        local temp_param_value = string.sub(t[2],1,-3)
+                        local t = explode(v, "\r\n\r\n")
+                        local temp_param_name = string.sub(t[1], 41, -2)
+                        local temp_param_value = string.sub(t[2], 1, -3)
                         args[temp_param_name] = temp_param_value
                     else
+                        is_have_file_param = true
                         --文件类型的参数，capture是参数名称，capture2是文件名
                         file_args[capture] = capture2
-                        table.insert(new_body_data,v)
+                        table.insert(new_body_data, v)
                     end
                 end
-                table.insert(new_body_data,1,first_string)
-                table.insert(new_body_data,last_string)
+                table.insert(new_body_data, 1, first_string)
+                table.insert(new_body_data, last_string)
                 --去掉app_key,app_secret等几个参数，把业务级别的参数传给内部的API
-                body_data = table.concat(new_body_data,boundary)--body_data可是符合http协议的请求体，不是普通的字符串
+                --body_data可是符合http协议的请求体，不是普通的字符串
+                body_data = table.concat(new_body_data, boundary)
             end
         else
+            ngx.log(ngx.ERR, "------非multipart/form-data类型的表单------")
             args = ngx.req.get_post_args()
+            is_have_file_param = false
+            file_args = {}
         end
     end
+    return args, is_have_file_param, file_args
 end
 
 function _M.split(self, s, delim)
